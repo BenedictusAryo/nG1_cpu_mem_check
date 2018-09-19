3531851dc789f2cbc07e9e3f60c46ead79f61e2f
Benedictus Aryo <benedictus.aryo@myoffice365.site>
2018-09-19 14:58:37 +0700

    Add Comment

--- cpu_mem_check.sh
+++ cpu_mem_check.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+#This script is purposed to get Average utilization CPU & Memory Usage
 HN=$(hostname)
+
 echo $HN
 sar | awk 'END{print "CPU Utilization : " 100-$8 "%"}'
 free -m | grep Mem | awk '{usage=100-(($4/$2)*100)} {print "Memory Usage : " usage "%"}'
